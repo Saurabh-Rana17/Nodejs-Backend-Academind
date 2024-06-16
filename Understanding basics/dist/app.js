5b4f495e8431973fa0c55ad0bf48c6fab901e4aa
gmr1289 <gmrgamer1289@gmail.com>
2024-06-16 11:17:33 +0530

    parsing request

--- Understanding basics/dist/app.js
+++ Understanding basics/dist/app.js
@@ -3,5 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+console.clear();
 const http_1 = __importDefault(require("http"));
-http_1.default.createServer((req, res) => { });
+const server = http_1.default.createServer((req, res) => {
+    const { headers, url, method } = req;
+    if (url === "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>Form</title></head>");
+        res.write("<body> <form method='POST' action='/message' > <input name='message' type='text'> <button type='submit'>Send </button>   </form> </body>");
+        res.write("</html>");
+        return res.end();
+    }
+    if (url === "/message" && method === "POST") {
+        res.write("<h1  >Welcome to message page</h1>");
+        res.end();
+    }
+});
+server.listen(3000);
--- Understanding basics/src/app.ts
+++ Understanding basics/src/app.ts
@@ -1,3 +1,40 @@
+console.clear();
 import http from "http";
+import fs from "fs";
 
-http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  const { headers, url, method } = req;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Form</title></head>");
+    res.write(
+      "<body> <form method='POST' action='/message' > <input name='message' type='text'> <button type='submit'>Send </button>   </form> </body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body: Uint8Array[] = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const data = parsedBody.split("=")[1];
+      fs.writeFileSync("./src/message.txt", data);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+
+    return res.end();
+  }
+
+  res.write("<H1>404 Page not found</h1>");
+  res.end();
+});
+
+server.listen(3000);
