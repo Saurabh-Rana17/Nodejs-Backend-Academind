ae8130117822fcd0c278e49cdc6eafe9d61e25a3
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-07-16 01:07:30 +0530

    served image statically

--- Working with Express.js/src/app.ts
+++ Working with Express.js/src/app.ts
@@ -61,6 +61,8 @@ const store: MongoDBStore = new mongoDBStore({
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "..", "public")));
+app.use("/images", express.static(path.join(__dirname, "..", "images")));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
